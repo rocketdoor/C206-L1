2f1dcab6ea0998c2c2b8f5a690341b92eb012508
rocketdoor <160641833+rocketdoor@users.noreply.github.com>
2024-03-04 23:10:42 -0300

    Aula 2

---
+++ PrimeiraAula/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Minha primeira aula de POO com Java" + "\n" + "Estamos usando o Netbeans!");
+    }
+}
